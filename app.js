2c74b8c0ce9c6d89ce6fae72ebfde037fde5709d
Samuel Mulkens <sammulkens@gmail.com>
2018-09-10 15:12:21 +0200

    creating tables with sequelize

--- app.js
+++ app.js
@@ -11,18 +11,49 @@ const sequelize = new Sequelize('blog', process.env.PGUSER, 'password', {
   host: 'localhost',
   dialect: 'postgres',
   operatorsAliases: false,
-
 })
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
+//creating the tables
+const Users = sequelize.define('users', {
+    firstName: {
+        type: Sequelize.STRING
+    },
+    lastName: {
+        type: Sequelize.STRING
+    },
+    passWord: {
+        type: Sequelize.STRING
+    }
+});
+
+Users.sync({force: true}).then(() => {
   })
 
+const Blogs = sequelize.define('blogs', {
+    title: {
+        type: Sequelize.STRING
+    },
+    body: {
+        type: Sequelize.STRING
+    }
+})
+
+Blogs.sync({force: true}).then(() => {
+})
+
+Blogs.belongsTo(Users)
+
+const Comments = sequelize.define('comments', {
+    comment: {
+        type: Sequelize.STRING
+    }
+})
+
+Comments.sync({force: true}).then(() => {
+})
+
+Comments.belongsTo(Users)
+Comments.belongsTo(Blogs)
 
 app.listen(3000, function() {
     console.log("Server is listening on port 3000")
