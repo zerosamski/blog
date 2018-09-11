8a5a4bf288a4a7d05499565f351d02ecc322bbec
Samuel Mulkens <sammulkens@gmail.com>
2018-09-11 09:36:05 +0200

    Added sessions and sequelizeStore

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
+//requiring modules
 const express = require('express');
-app = express();
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const Sequelize = require('sequelize');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-app.set("view enginge", "ejs")
+//configuring modules
+app = express();
+app.set("view engine", "ejs")
+
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
 
-const Sequelize = require('sequelize');
+
 const sequelize = new Sequelize('blog', process.env.PGUSER, 'password', {
   host: 'localhost',
   dialect: 'postgres',
-  operatorsAliases: false,
+  operatorsAliases: false
 })
 
-//creating the tables
+//model definitions
 const Users = sequelize.define('users', {
     firstName: {
         type: Sequelize.STRING
@@ -54,6 +60,26 @@ Comments.sync({force: true}).then(() => {
 
 Comments.belongsTo(Users)
 Comments.belongsTo(Blogs)
+
+//set up sessions
+app.use(session({
+    secret: "canihazcheeseburger",
+    store: new SequelizeStore({
+        db: sequelize
+    }),
+    resave: false,
+    saveUninitialized: false,
+
+}))
+
+
+app.get("/", (req, res) => {
+    res.render("home")
+})
+
+app.get("/secret", (req, res) => {
+    res.render("secret")
+})
 
 app.listen(3000, function() {
     console.log("Server is listening on port 3000")
